92cf6fc05d83020a591cbd8624226dfe9da53323
sparkbold-git <sparkboldstudio@gmail.com>
2018-10-13 16:15:41 -0500

    add json npm and node-fetch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
+import fetch from "node-fetch";
 document.addEventListener('DOMContentLoaded', () => {
   console.log(POKEMON)
   //YOUR CODE HERE
+  fetchInitialPokemon()
 })
+
+const render = (pokemon) => {
+  let container = document.getElementById('pokemon-container')
+  container.innerHTML +=
+    `<div class="pokemon-cards" id="${pokemon.id}">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <h2>${pokemon.name}</h2>
+      <img src="${pokemon.sprites.front}"/>
+    </div>
+      <div class="flip-card-back">
+      <h2>${pokemon.name}</h2>
+      <img src="${pokemon.sprites.back}" />
+      </div>
+    </div>
+  </div>`
+}
+
+const fetchInitialPokemon = () => {
+  fetch('http://localhost:3000/pokemon')
+    .then(resp => resp.json())
+    .then(data => data.forEach(pokemon => {
+      render(pokemon)
+    }))
+}
+
+const result = fetchInitialPokemon()
