6eceaf3cfbfd222f9741e579912fb5d7e5a6194a
sparkbold-git <sparkboldstudio@gmail.com>
2018-10-14 01:21:35 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -52,11 +52,11 @@ const searchPokemon = () => {
     const filterPokemons = pokemons.filter(pokemon => pokemon.name.includes(keyword));
     let container = document.getElementById('pokemon-container');
     if (filterPokemons.length) {
-      document.getElementById("p1").innerHTML = `Yay! Hera are <strong>${filterPokemons.length}</strong> Pokémon name with keyword <strong><em>'${keyword}'</em>'</strong>`;
+      document.getElementById("p1").innerHTML = `Yay! Here are <strong>${filterPokemons.length}</strong> Pokémon with name includes <strong><em>'${keyword}'</em>'</strong>`;
       container.innerHTML = "";
       filterPokemons.forEach(pokemon => render(pokemon))
     } else {
-      document.getElementById("p1").innerHTML = `Hmm! I can't find any Pokémon to be found with keyword <strong><em>'${keyword}'</em>'</strong>. Try again!`
+      document.getElementById("p1").innerHTML = `Hmm! I can't find any Pokémon with name includes <strong><em>'${keyword}'</em></strong>. Try again!`
       container.innerHTML = ""
     }
   })
