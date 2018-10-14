b936c717b465696adce3336447c2bb72caa3de05
sparkbold-git <sparkboldstudio@gmail.com>
2018-10-13 22:14:55 -0500

    render flip with onclick

--- src/index.js
+++ src/index.js
@@ -1,33 +1,67 @@
-import fetch from "node-fetch";
+let pokemons;
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log(POKEMON)
+  // console.log(POKEMON)
   //YOUR CODE HERE
-  fetchInitialPokemon()
+
+ 
+  // CSS hover flip
+  // const render = (pokemon) => {
+  //   let container = document.getElementById('pokemon-container')
+  //   container.innerHTML +=
+  //     `<div class="flip-card" id="${pokemon.id}">
+  //       <div class="flip-card-inner">
+  //         <div class="flip-card-front">
+  //           <h2>${pokemon.name}</h2>
+  //           <img src="${pokemon.sprites.front}" />
+  //         </div>
+  //         <div class="flip-card-back">
+  //           <h4>${pokemon.abilities}</h4>
+  //           <img src="${pokemon.sprites.back}" />
+  //         </div>
+  //       </div>
+  //     </div>`
+  // }
+
+  // onClick flip
+  const render = (pokemon) => {
+    let container = document.getElementById('pokemon-container')
+    container.innerHTML +=
+      `<div class="pokemon-cards" id="${pokemon.id}" onClick="flipFunc(${pokemon.id})">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <h2>${pokemon.name}</h2>
+            <img src="${pokemon.sprites.front}" />
+          </div>
+          <div class="flip-card-back">
+            <h4>${pokemon.abilities}</h4>
+            <img src="${pokemon.sprites.back}" />
+          </div>
+        </div>
+      </div>`
+  }
+
+  const fetchInitialPokemon = () => {
+    fetch('db.json')
+      .then(resp => resp.json())
+      .then(data => {
+        pokemons = data;
+        document.getElementById("p1").innerHTML = `There are <strong>${pokemons.length}</strong> Pokémon here`;
+        pokemons.forEach(pokemon => {
+        render(pokemon);
+        })})
+  }
+
+  const flipFunc = (id) => {
+    let inner = document.getElementById(id).getElementsByClassName('flip-card-inner');
+    let cardTransform = inner[0].style.transform
+    if (cardTransform === "" || cardTransform === 'rotateY(0deg)') {
+        inner[0].style.transform = 'rotateY(180deg)';
+      } else {
+        inner[0].style.transform = 'rotateY(0deg)';
+      }
+  }
+
+  fetchInitialPokemon();
+
 })
-
-const render = (pokemon) => {
-  let container = document.getElementById('pokemon-container')
-  container.innerHTML +=
-    `<div class="pokemon-cards" id="${pokemon.id}">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <h2>${pokemon.name}</h2>
-      <img src="${pokemon.sprites.front}"/>
-    </div>
-      <div class="flip-card-back">
-      <h2>${pokemon.name}</h2>
-      <img src="${pokemon.sprites.back}" />
-      </div>
-    </div>
-  </div>`
-}
-
-const fetchInitialPokemon = () => {
-  fetch('http://localhost:3000/pokemon')
-    .then(resp => resp.json())
-    .then(data => data.forEach(pokemon => {
-      render(pokemon)
-    }))
-}
-
-const result = fetchInitialPokemon()
